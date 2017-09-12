07a886441ef5448ae8b74f68fcaf409633a8349a
felce <zabara.alexey@gmail.com>
2017-09-12 14:09:26 +0300

    st3

--- st3/add_to_json.php
+++ st3/add_to_json.php
@@ -4,6 +4,9 @@
     $jsonData = json_decode(file_get_contents($data), true);
     $index = count($jsonData);
     $jsonData[$index]['index'] = '';
+    $jsonData[$index]['text'] = '';
+    $jsonData[$index]['coords'][0] = 0;
+    $jsonData[$index]['coords'][1] = 0;
 
     $newJsonString = json_encode($jsonData, JSON_PRETTY_PRINT);
     file_put_contents($data, $newJsonString);
--- st3/delete_from_json.php
+++ st3/delete_from_json.php
@@ -3,6 +3,9 @@
     $data = 'data.json';
     $jsonData = json_decode(file_get_contents($data), true);
     $index = $_POST['index'];
-    $jsonData[$index] = '';
+    $jsonData[$index]['index'] = '';
+    $jsonData[$index]['text'] = '';
+    $jsonData[$index]['coords'][0] = 0;
+    $jsonData[$index]['coords'][1] = 0;
     $newJsonString = json_encode($jsonData, JSON_PRETTY_PRINT);
     file_put_contents($data, $newJsonString);
